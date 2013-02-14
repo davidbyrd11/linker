5cbfedae0db5895ec2995789919defe9794ae477
David Byrd <david@byrdhou.se>
2013-02-14 15:10:52 -0500

    started playing with c and pythong

--- app.js
+++ app.js
@@ -1,125 +1,3 @@
-function parseModules () {
-
-  var modules = [
-  {
-    definitionMap:{
-      "xy": 2
-    },
-    usageMap:{
-      "z": 4
-    },
-    programInstructions:[
-      {
-        opcode: 1,
-        address: 4,
-        type: "R"
-      },
-      {
-        opcode: 5,
-        address: 678,
-        type: "I"
-      },
-      {
-        opcode: 2,
-        address: 777,
-        type: "E"
-      },
-      {
-        opcode: 8,
-        address: 2,
-        type: "R"
-      },
-      {
-        opcode: 7,
-        address: 2,
-        type: "E"
-      }
-    ]
-  },
-  {
-    definitionMap:{},
-    usageMap:{
-      "z": 3
-    },
-    programInstructions:[
-      {
-        opcode: 8,
-        address: 1,
-        type: "R"
-      },
-      {
-        opcode: 1,
-        address: 777,
-        type: "E"
-      },
-      {
-        opcode: 1,
-        address: 1,
-        type: "E"
-      },
-      {
-        opcode: 3,
-        address: 2,
-        type: "E"
-      },
-      {
-        opcode: 1,
-        address: 2,
-        type: "R"
-      },
-      {
-        opcode: 1,
-        address: 10,
-        type: "A"
-      }
-    ]
-  },
-  {
-    definitionMap:{},
-    usageMap:{
-      "z": 1
-    },
-    programInstructions:[
-      {
-        opcode: 5,
-        address: 1,
-        type: "R"
-      },
-      {
-        opcode: 4,
-        address: 777,
-        type: "E"
-      }
-    ]
-  },
-  {
-    definitionMap:{
-      "z": 2
-    },
-    usageMap: {
-      "xy": 2
-    },
-    programInstructions: [
-      {
-        opcode: 8,
-        address: 0,
-        type: "A"
-      },
-      {
-        opcode: 1,
-        address: 777,
-        type: "E"
-      },
-      {
-        opcode: 2,
-        address: 1,
-        type: "E"
-      }
-    ]
-  }];
-  return modules;
-}
-
 /**
  * Generate symbol table
  */
@@ -150,6 +28,7 @@ function getMemoryMap (ms, symbolTable) {
   var memoryMap = {}, // a map of index: (opcode*1000) + address
       count = 0,      // the num of the instruction we're on
       offset = 0;     // the offset for relative addresses
+  
   for (var i = 0; i < ms.length; i++) {
     for (var use in ms[i].usageMap) {
       var indexOfUse = ms[i].usageMap[use];
@@ -170,7 +49,6 @@ function getMemoryMap (ms, symbolTable) {
           ms[i].programInstructions[indexOfUse].type = "E";
         }
 
-
         var tmp = ms[i].programInstructions[indexOfNextUse].address;
         ms[i].programInstructions[indexOfNextUse].address = useValue;
         indexOfNextUse = tmp;
@@ -181,23 +59,12 @@ function getMemoryMap (ms, symbolTable) {
     for (var j = 0; j < ms[i].programInstructions.length; j++) {
       var instruction = ms[i].programInstructions[j];
 
-      switch (instruction.type) {
-        case "I":
-          memoryMap[count] = (instruction.opcode * 1000) + instruction.address;
-        break;
-        case "A":
-          memoryMap[count] = (instruction.opcode * 1000) + instruction.address;
-        break;
-        case "R":
-          memoryMap[count] = (instruction.opcode * 1000) + instruction.address + offset;
-        break;
-        case "E":
-          memoryMap[count] = (instruction.opcode * 1000) + instruction.address;//already been adjusted
-        break;
-        default:
-          console.log("Error: Invalid Letter");
-
-      }
+      if (instruction.type == "I" || instruction.type == "A" || instruction.type == "E")
+        memoryMap[count] = (instruction.opcode * 1000) + instruction.address;
+      else if (instruction.type == "R")
+        memoryMap[count] = (instruction.opcode * 1000) + instruction.address + offset;
+      else console.log("Error: Invalid Letter");
+      
       count++;
     }
     offset = count; //relative addresses are adjusted by the size of modules before them, not the instructions in the same module
@@ -205,8 +72,7 @@ function getMemoryMap (ms, symbolTable) {
   return memoryMap;
 }
 
-
-var modules = parseModules();
+var modules = require('./lib/modules')();
 var symbolTable = getSymbolTable(modules);
 console.log("Symbol Table");
 console.log(symbolTable);
---
+++ app.py
@@ -0,0 +1,125 @@
+def parseModules():
+  return [
+  {
+    "definitionMap": {
+      "xy": 2
+    },
+    "usageMap":{
+      "z": 4
+    },
+    "programInstructions":[
+      {
+        "opcode": 1,
+        "address": 4,
+        "type": "R"
+      },
+      {
+        "opcode": 5,
+        "address": 678,
+        "type": "I"
+      },
+      {
+        "opcode": 2,
+        "address": 777,
+        "type": "E"
+      },
+      {
+        "opcode": 8,
+        "address": 2,
+        "type": "R"
+      },
+      {
+        "opcode": 7,
+        "address": 2,
+        "type": "E"
+      }
+    ]
+  },
+  {
+    "definitionMap":{},
+    "usageMap":{
+      "z": 3
+    },
+    "programInstructions":[
+      {
+        "opcode": 8,
+        "address": 1,
+        "type": "R"
+      },
+      {
+        "opcode": 1,
+        "address": 777,
+        "type": "E"
+      },
+      {
+        "opcode": 1,
+        "address": 1,
+        "type": "E"
+      },
+      {
+        "opcode": 3,
+        "address": 2,
+        "type": "E"
+      },
+      {
+        "opcode": 1,
+        "address": 2,
+        "type": "R"
+      },
+      {
+        "opcode": 1,
+        "address": 10,
+        "type": "A"
+      }
+    ]
+  },
+  {
+    "definitionMap":{},
+    "usageMap":{
+      "z": 1
+    },
+    "programInstructions":[
+      {
+        "opcode": 5,
+        "address": 1,
+        "type": "R"
+      },
+      {
+        "opcode": 4,
+        "address": 777,
+        "type": "E"
+      }
+    ]
+  },
+  {
+    "definitionMap":{
+      "z": 2
+    },
+    "usageMap": {
+      "xy": 2
+    },
+    "programInstructions": [
+      {
+        "opcode": 8,
+        "address": 0,
+        "type": "A"
+      },
+      {
+        "opcode": 1,
+        "address": 777,
+        "type": "E"
+      },
+      {
+        "opcode": 2,
+        "address": 1,
+        "type": "E"
+      }
+    ]
+  }]
+
+for m in parseModules():
+  for key in m:
+    if key is "usageMap":
+      print(m[key]["z"])
+
+
