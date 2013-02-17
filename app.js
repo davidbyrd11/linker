1910dfe29729b47cca2c268c5798aeea75d5fe29
David Byrd <david@byrdhou.se>
2013-02-17 06:41:27 -0500

    made app an executable

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+#!/usr/bin/env node
 !function (file) {
   var modules = require('./lib/reader')(file),
       linker = require('./lib/linker'),
       symbolTable = linker.getSymbolTable(modules);
-      
+
   console.log("Symbol Table");
   console.log(symbolTable);
   console.log("Memory Map");
