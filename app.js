077817c1748bfc1888648d1c8d700d8c176a5518
David Byrd <david@byrdhou.se>
2013-02-18 21:52:54 -0500

    all tests passing

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
       linker = require('./lib/linker'),
       symbolTable = linker.getSymbolTable(modules);
 
-  console.log("Symbol Table");
+  console.log("Symbol Table:");
   console.log(symbolTable);
-  console.log("Memory Map");
+  console.log("Memory Map:");
   console.log(linker.getMemoryMap(modules, symbolTable));
 }(process.argv.slice(2)[0])
